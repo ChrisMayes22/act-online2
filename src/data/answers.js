9fbdf126e7a4248620b8a0c0a1637e39cb0ad9de
Chris Mayes <coachchristac@gmail.com>
2019-11-16 00:52:36 -0500

    Refactored aspects of front-end, worked on functions for checking right / wrong answers

---
+++ src/data/answers.js
@@ -0,0 +1,6 @@
+const science71C = [
+    'A','G','A','H','D','J','D','H','C','F','D','F','C','G','C','G','A','F','A','G',
+    'C','G','D','F','D','G','D','G','A','H','A','J','B','F','C','G','C','H','B','J'
+];
+
+export default science71C;
